9d25bfbc81b253ed090034e0453632df31339f17
Ian Waudby-Smith <ianwaudby-smith@live.ca>
2015-09-19 23:57:40 -0400

    Download tweets on day-to-day basis (just for hackathon sake) and organize data in controller

--- psephologyapp/app/controllers/welcome_controller.rb
+++ psephologyapp/app/controllers/welcome_controller.rb
@@ -1,5 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-  	gon.tweets = Tweet.limit(10)
+  	ndpTweetsArray = []
+  	cpcTweetsArray = []
+  	lpcTweetsArray = []
+  	(Date.new(2015, 9, 13)..Date.new(2015, 9, 20)).each do |date|
+	  ndpTweets = Tweet.where(:tweeted_at => date.beginning_of_day..date.end_of_day).where(:party => "ndp")
+	  cpcTweets = Tweet.where(:tweeted_at => date.beginning_of_day..date.end_of_day).where(:party => "cpc")
+	  lpcTweets = Tweet.where(:tweeted_at => date.beginning_of_day..date.end_of_day).where(:party => "lpc")
+	  ndpTweetsArray.push({:date => date, :sentiment => ndpTweets.average(:sentiment)})
+	  cpcTweetsArray.push({:date => date, :sentiment => cpcTweets.average(:sentiment)})
+	  lpcTweetsArray.push({:date => date, :sentiment => lpcTweets.average(:sentiment)})
+	end
+	gon.ndpTweets = ndpTweetsArray
+	gon.cpcTweets = cpcTweetsArray
+	gon.lpcTweets = lpcTweetsArray
   end
 end
